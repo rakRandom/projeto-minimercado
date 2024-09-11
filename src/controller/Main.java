bc1bf61f40e5344cd3885184812057a152ca921a
Fellipe Leonardo <112525075+rakRandom@users.noreply.github.com>
2024-09-10 21:19:54 -0300

    initial commit

---
+++ src/controller/Main.java
@@ -0,0 +1,24 @@
+package controller;
+
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import view.FrmMain;
+
+/**
+ *
+ * @author Fellipe Leonardo
+ */
+public class Main {
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } 
+        catch (UnsupportedLookAndFeelException | 
+                ClassNotFoundException | 
+                InstantiationException | 
+                IllegalAccessException e) {}
+        
+        var main = new FrmMain();
+        main.setVisible(true);
+    }
+}
